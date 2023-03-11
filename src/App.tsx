87809996e94c7edcc5460027c6381304defc22bd
Mike Flood <mstuartflood@gmail.com>
2023-03-11 14:53:07 +0000

    nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
 import React from "react";
 import "./App.css";
-import { Redirect, Route, Router, Switch } from "react-router-dom";
+import { Link, Redirect, Route, Router, Switch } from "react-router-dom";
 import { createMemoryHistory } from "history";
 import Prospect from "./components/views/Prospect";
 import Prompts from "./components/views/Prompts";
+import NavBar from "./components/molecules/NavBar";
 
 const history = createMemoryHistory();
 
 function App() {
   return (
-    <div className="w-full h-full px-8 py-6">
+    <div className="w-full h-full">
       <div className="h-full w-full">
         <Router history={history}>
-          <Switch>
-            <Route path="/prospect">
-              <Prospect />
-            </Route>
-            <Route path="/prompts">
-              <Prompts />
-            </Route>
-            <Route path="*">
-              <Redirect to="/prospect" />
-            </Route>
-          </Switch>
+          <NavBar />
+          <div className="px-8 py-6">
+            <Switch>
+              <Route path="/prospect">
+                <Prospect />
+              </Route>
+              <Route path="/prompts">
+                <Prompts />
+              </Route>
+              <Route path="*">
+                <Redirect to="/prospect" />
+              </Route>
+            </Switch>
+          </div>
         </Router>
       </div>
     </div>
---
+++ src/components/molecules/NavBar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const NavBarItem = ({ to, text }: { to: string; text: string }) => {
+  const { pathname } = useLocation();
+  return (
+    <Link
+      to={to}
+      className={`inline-block p-4 w-full flex items-center justify-center border-b-2 ${
+        to === pathname
+          ? "border-blue-600 text-blue-600"
+          : "text-gray-500 border-transparent hover:text-gray-600 hover:border-gray-300"
+      }`}
+    >
+      {text}
+    </Link>
+  );
+};
+
+const NavBar = () => {
+  return (
+    <div className="border-b">
+      <ul className="grid grid-cols-2">
+        <li>
+          <NavBarItem to="/prospect" text="Prospects" />
+        </li>
+        <li>
+          <NavBarItem to="/prompts" text="Prompts" />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
