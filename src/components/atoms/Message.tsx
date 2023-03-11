bb52d29489a1b5f309d79d433cbd37c31c825134
Mike Flood <mstuartflood@gmail.com>
2023-03-11 14:31:22 +0000

    copy feedback

--- src/components/atoms/Message.tsx
+++ src/components/atoms/Message.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useSelector } from "react-redux";
 
 interface MessageProps {
   text: string;
 }
 
 const Message = ({ text }: MessageProps) => {
+  const [showCopied, setShowCopied] = useState(false);
+
+  const onCopy = () => {
+    setShowCopied(true);
+    setTimeout(() => {
+      setShowCopied(false);
+    }, 2000);
+  };
+
   return (
-    <CopyToClipboard text={text} onCopy={() => {}}>
-      <button className="text-left">{text}</button>
+    <CopyToClipboard text={text} onCopy={onCopy}>
+      <button className="text-left group border p-2">
+        <div>{text}</div>
+        <div className="h-4">
+          {showCopied ? (
+            <>Copied!</>
+          ) : (
+            <div className="hidden group-hover:block">Click to copy</div>
+          )}
+        </div>
+      </button>
     </CopyToClipboard>
   );
 };
