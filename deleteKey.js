2b9cae09790608229eaf7f23308991a347f293ca
Mike Flood <mstuartflood@gmail.com>
2023-03-15 10:30:40 +0000

    build

--- deleteKey.js
+++ deleteKey.js
@@ -1,12 +1,5 @@
 const fs = require("fs");
 const fileName = "./build/manifest.json";
-//
-fs.readdir("./build", (err, files) => {
-  console.log("contents:");
-  files.forEach((file) => {
-    console.log(file);
-  });
-});
 
 const file = require(fileName);
 
