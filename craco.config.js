4dfae475432dfe231f05835fa66eaa3d97b29845
Mike Flood <mstuartflood@gmail.com>
2023-03-27 19:21:08 +0100

    v1 detecting events and applying styling

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,22 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   webpack: {
     configure: (webpackConfig, { env, paths }) => {
       // console.log(webpackConfig);
       return {
         ...webpackConfig,
+        plugins: [
+          ...webpackConfig.plugins.map((plugin) => {
+            if (plugin.constructor.name !== "MiniCssExtractPlugin") {
+              return plugin;
+            }
+            return new MiniCssExtractPlugin({
+              ...plugin.options,
+              filename: "static/css/[name].css",
+            });
+          }),
+        ],
         entry: {
           main: [
             env === "development" &&
@@ -11,6 +24,7 @@ module.exports = {
             paths.appIndexJs,
           ].filter(Boolean),
           background: "./src/chrome/background.ts",
+          msgListener: "./src/chrome/msgListener.ts",
         },
         output: {
           ...webpackConfig.output,
---
+++ src/chrome/msgListener.ts
@@ -0,0 +1,57 @@
+import { Toast } from "../toast/toast";
+
+console.log("tiramisu loaded");
+
+export const addListeners = () => {
+  document.addEventListener("submit", (event) => {
+    if (!event.target) {
+      return;
+    }
+    const form = event.target as HTMLFormElement;
+    if (!form.className.includes("msg-form")) {
+      console.log("submit event did not come from a message form");
+      console.log(form.className);
+      return;
+    }
+    const input = form.querySelector(".msg-form__contenteditable");
+    if (!input) {
+      console.log("no div input found inside message form");
+      return;
+    }
+    // const msg = (input as HTMLDivElement).innerText;
+
+    let wrapper;
+    let parent: HTMLElement = form;
+    while (!wrapper) {
+      if (!parent.parentElement) {
+        break;
+      }
+      parent = parent.parentElement;
+      if (parent.className.includes("msg-convo-wrapper")) {
+        wrapper = parent;
+      }
+    }
+    if (!wrapper) {
+      console.log("no wrapper with matching class");
+      return;
+    }
+    const recipient = wrapper.querySelector("h2");
+    if (!recipient) {
+      console.log("no h2 inside wrapper");
+      return;
+    }
+    const recipientName = recipient.innerText;
+
+    new Toast({
+      message: `Record this message to ${recipientName} Salesforce?`,
+      customButtons: [
+        {
+          text: "Yes",
+          onClick: () => console.log("save"),
+        },
+      ],
+    });
+  });
+};
+
+addListeners();
