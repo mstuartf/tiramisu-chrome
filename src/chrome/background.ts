8ef5668d35a91339c1dc3a6a04395e33757b2a88
Mike Flood <mstuartflood@gmail.com>
2023-03-29 09:17:25 +0100

    update checks

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -1,4 +1,10 @@
-import {wrapStore} from 'webext-redux';
-import {store} from "../redux/store";
+import { wrapStore } from "webext-redux";
+import { store } from "../redux/store";
 
 wrapStore(store);
+
+// https://stackoverflow.com/a/15842031
+chrome.runtime.onUpdateAvailable.addListener(function (details) {
+  console.log("updating to version " + details.version);
+  chrome.runtime.reload();
+});
--- src/components/views/Account.tsx
+++ src/components/views/Account.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Btn from "../atoms/Btn";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../redux/user/slice";
@@ -11,8 +11,25 @@ const Account = () => {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
 
+  const [update, setUpdate] = useState("No updates found.");
+  const [isChecking, setIsChecking] = useState(false);
+
   const onLogout = () => {
     dispatch(logout());
+  };
+
+  const checkForUpdates = () => {
+    setIsChecking(true);
+    chrome.runtime.requestUpdateCheck(function (status) {
+      setIsChecking(false);
+      if (status === "update_available") {
+        setUpdate("Update pending...");
+      } else if (status === "no_update") {
+        setUpdate("No update found.");
+      } else if (status === "throttled") {
+        setUpdate("Please try again later.");
+      }
+    });
   };
 
   if (!user) {
@@ -26,6 +43,12 @@ const Account = () => {
       <div className="flex justify-between items-center">
         <div>You are currently running version {packageJson.version}.</div>
         <Btn onClick={onLogout}>Logout</Btn>
+      </div>
+      <div className="flex justify-between items-center">
+        <div>{update}</div>
+        <Btn onClick={checkForUpdates} disabled={isChecking}>
+          Check for updates
+        </Btn>
       </div>
       {admin && <TeamContainer />}
     </div>
